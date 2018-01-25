09313b583239fa5cbd169f9d236f9196f341d657
Antoine C <antoine38100@gmail.com>
2018-01-25 16:04:34 +0100

    First commit

---
+++ lab1/src/CalculusInterface.java
@@ -0,0 +1,8 @@
+package com.idslab.labone;
+
+interface Calculator_itf {
+  public int plus(int, int);
+  public int minus (int, int);
+  public int divide(int, int);
+  public int multiply(int, int);
+}
