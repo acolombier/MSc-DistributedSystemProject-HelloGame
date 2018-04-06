25cec36af861d90fc5e34ea5635378e903521805
Antoine <antoine38100@gmail.com>
2018-04-06 21:49:58 +0200

    Refactoring, adding keep alive, handling basic errors, fixing deadlock because of some message no acked, but still no player...

--- project/server/main.py
+++ project/server/main.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python3
 import pika
 from package.area import Area
-
+import argparse
         
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Hello Game server launcher.')
+    
+    parser.add_argument('--single-node', dest='node_id', default=1, type=int, nargs='?', help='Launch a single node with the given id')
+    parser.add_argument('rmq_host', type=str, default='localhost',
+                help='Server name that run RabbitMQ')
+    parser.add_argument('width', type=int, default=2,
+                help='Number of Area on a row')
+    parser.add_argument('height', type=int, default=2,
+                help='Number of Area on a col')
+    args = parser.parse_args()
+    
+    
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(args.rmq_host))
     channel = connection.channel()
     
-    a = [Area(i, channel) for i in range(0, 4)]
-    a[0].run()
+    
+    if (args.node_id == -1):
+        [Area(i, channel) for i in range(0, 4)][0].run()
+    else:
+        Area(args.node_id, channel).run()
--- project/server/package/area.py
+++ project/server/package/area.py
@@ -2,7 +2,10 @@ import pika
 from . import model
 from .serializer import *
 from random import randint
-
+import uuid
+from time import time
+from threading import Timer
+from copy import copy
 
 class Area:
     def __init__(self, _id, channel):
@@ -12,11 +15,9 @@ class Area:
         
         self.channel = channel
         
-        #~ channel.queue_declare(queue='connect')
         channel.queue_declare(queue='main_queue')
         channel.basic_qos(prefetch_count=1)
         channel.basic_consume(self.on_request, queue='main_queue')
-        #~ channel.basic_consume(self.on_request, queue='connect')
     
         ### Declaring Broadcast
         channel.exchange_declare(exchange='broadcast',
@@ -33,26 +34,52 @@ class Area:
         channel.queue_bind(exchange='direct',
                            queue="main_queue",
                            routing_key='main_queue')
+        
+        self.player_alive_check() 
 
+    def player_alive_check(self):
+        # Make a local copy of the current state, in case of someone arrives during the cleaning routing (RuntimeError)
+        players = copy(self.players)
+        
+        for pos, player in players.items():
+            if player.last_activity + 6 < time():
+                self.player_disconnect(player, model.Player.DISCONNECT_TIMEOUT)
+        
+        self.player_ka_timer = Timer(6.0, self.player_alive_check)
+        self.player_ka_timer.start() 
+        
+    def player_disconnect(self, player, r=model.Player.DISCONNECT_QUIT):        
+        del self.players[player.position]        
+        self.channel.basic_publish(exchange='broadcast',
+                         routing_key='',
+                         body=json_encode(model.Event(model.Event.QUIT, player=player, reason=r)))
 
     def on_request(self, ch, method, props, body):
         data = json_decode(body)
+                
+        print("On request: %s" % body)
+        
+        if hasattr(data, 'player') and data.player.uuid in self.players.keys():
+            self.players[data.player].last_activity = time()
         
         if isinstance(data, model.MoveRequest):
-            if data.player not in self.players.keys():
-                print("Error, unknown player")
-                return
-                
+            # Destination is away, we forward the request
             if data.area() != self.id:
                 ch.basic_publish(exchange='direct',
                                  routing_key='node_area_%d.*' % data.area,
                                  properties=pika.BasicProperties(reply_to = props.reply_to),
                                  body=body)
-            else:
-                if data.cellid() not in self.players.values():
-                    self.players[data.player] = data.cellid()
+            else:                
+                if data.player not in self.players.values():
+                    print("Error, unknown player")
+                    
+                # Destination is free, we accept the move
+                elif data.cellid() not in self.players.keys():
+                    self.players[data.cellid()] = data.player
+                    self.players[data.cellid()].last_activity = time()
                     
                     ch.basic_publish(exchange='broadcast',
+                                     routing_key='',
                                      body=json_encode(model.Event(model.Event.PLAYER_MOVE, data.player, data.destination)))
                     # TODO: say hi if some is nearby
                 else:
@@ -67,14 +94,16 @@ class Area:
                 ch.basic_nack(delivery_tag = method.delivery_tag)
                 
             c = randint(0, 15)
-            while c in self.players.values():
+            while c in self.players.keys():
                 c = randint(0, 15)
                 
             print("Accepting new client '%s' at pos %s" % (data.player.nickname, c))
                 
-            self.players[data.player] = c
             data.player.area = self.id
             data.player.position = c
+            data.player.uuid = str(uuid.uuid4())
+            self.players[c] = data.player
+            self.players[c].last_activity = time()
             
             ch.basic_publish(exchange='',
                              routing_key=props.reply_to,
@@ -90,6 +119,14 @@ class Area:
                                 model.Event(model.Event.PLAYER_JOIN, 
                                             player=data.player)))
             
+            ch.basic_ack(delivery_tag = method.delivery_tag)
+        elif isinstance(data, model.Event):
+            if data.type == model.Event.QUIT and data.player in self.players.values():
+                self.player_disconnect(data.player, model.Player.DISCONNECT_QUIT)
+            if data.type == model.Event.KEEP_ALIVE and data.player in self.players.values() \
+                and self.players[data.player.position] == data.player:
+                self.players[data.player.position].last_activity = time()
+                
             ch.basic_ack(delivery_tag = method.delivery_tag)
 
     def run(self):
